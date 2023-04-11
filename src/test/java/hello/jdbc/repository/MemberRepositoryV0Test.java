e6c551663f1042ecfea73d7756e8ab3f298ec8b3
sunwooKim <frod5@kakao.com>
2023-04-11 15:56:53 +0900

    스프링 DB접근기술1 - JDBC를 이용해 회원데이터 수정,삭제 실습 및 테스트 코드 검증

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 public class MemberRepositoryV0Test {
@@ -15,12 +17,22 @@ public class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("memberV3", 10000);
+        Member member = new Member("memberV5", 10000);
         repository.save(member);
 
         //findById
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember = {}",findMember);
         assertThat(findMember).isEqualTo(member); // member랑 findMember랑 객체 인스턴스(주소값)는 다르다. 그런데 Member클래스에 @Data (Equals, HashCode)를 만들어줘서 같다.
+
+        //update : money 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
