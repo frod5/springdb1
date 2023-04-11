2b08b62e9e5d138561e4987bd0821e8a6258e19b
sunwooKim <frod5@kakao.com>
2023-04-11 14:29:01 +0900

    스프링 DB접근기술1 - JDBC를 이용해 회원데이터 등록 실습 및 테스트

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,66 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+
+/**
+ * JDBC - DriverManager 사용
+ */
+@Slf4j
+public class MemberRepositoryV0 {
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values(?,?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();  //db 커넥션
+            pstmt = con.prepareStatement(sql);  //sql 문장 생성
+            pstmt.setString(1, member.getMemberId()); // 파라미터 세팅
+            pstmt.setInt(2,member.getMoney());
+            pstmt.executeUpdate(); // SQL 실행
+            return member;
+        } catch (SQLException e) {
+            log.info("db error",e);
+            throw e;
+        } finally {
+            close(con,pstmt,null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+        //역순으로 close
+
+        if(rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.error("error",e);
+            }
+        }
+
+        if(stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.error("error",e);
+            }
+        }
+
+        if(con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.error("error",e);
+            }
+        }
+    }
+
+    private Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+}
