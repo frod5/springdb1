268e8cb5543be7e58980a5e751fed747f20eff9d
sunwooKim <frod5@kakao.com>
2023-04-12 15:52:01 +0900

    스프링 DB접근기술1 - DB 트랜잭션 - @Transactional AOP 적용 및 테스트코드 실습. (서비스 계층에는 이제 비지니스 로직만 존재)

---
+++ src/test/java/hello/jdbc/service/MemberServiceV3_3Test.java
@@ -0,0 +1,123 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * 트랜잭션 - 커넥션 파라미터 전달 방식 동기화.
+ */
+@Slf4j
+@SpringBootTest  //기존에는 스프링을 띄워서 테스트한것이 아니어서 @Transactional이 동작히지 않는다. 그래서 스프링테스트를 하기위해 선언.
+public class MemberServiceV3_3Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    @Autowired
+    private MemberRepositoryV3 memberRepository;
+
+    @Autowired
+    private MemberServiceV3_3 memberService;
+
+    @TestConfiguration  //스프링 테스트를 하기위해 빈 등록
+    static class TestConfig {
+        @Bean
+        DataSource dataSource () {
+            return new DriverManagerDataSource(URL,USERNAME,PASSWORD);
+        }
+        @Bean
+        PlatformTransactionManager transactionManager() {
+            return new DataSourceTransactionManager(dataSource());
+        }
+        @Bean
+        MemberRepositoryV3 memberRepositoryV3() {
+            return new MemberRepositoryV3(dataSource());
+        }
+        @Bean
+        MemberServiceV3_3 memberServiceV3_3() {
+            return new MemberServiceV3_3(memberRepositoryV3());
+        }
+    }
+    @AfterEach
+    void afterEach() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    void aopCheck() {
+        log.info("memberService class= {}",memberService.getClass());
+        //로그 : memberService class= class hello.jdbc.service.MemberServiceV3_3$$EnhancerBySpringCGLIB$$613aeb5c
+        //EnhancerBySpringCGLIB를 보면 프록시가 생성된것이다.
+        //프록시는 스프링이 실제 memberService를 사용하는것이아니라 프록시가 만든 memberService를 사용한다.
+        //프록시가 만든 memberService안에는 실제 memberService가 주입되어, 트랜잭션 로직 수행하고 주입되어있는 memberService의 비지니스로직을 호출한다.
+        log.info("memberRepository class= {}",memberRepository.getClass());
+
+        assertThat(AopUtils.isAopProxy(memberService)).isTrue();  //AOP 적용됬는지 확인 AopUtils.isAopProxy
+        assertThat(AopUtils.isAopProxy(memberRepository)).isFalse();  // repository는 aop관련 소스를 사용하지않아 프록시 객체가 생성되지않아 false
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        //when
+        memberService.accountTransfer(memberA.getMemberId(),memberB.getMemberId(), 2000);
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체중 예외 발생")
+    void accountTransferEx() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+        //when
+        assertThatThrownBy(()-> memberService.accountTransfer(memberA.getMemberId(),memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberEx.getMemberId());
+
+        assertThat(findMemberA.getMoney()).isEqualTo(10000);
+        assertThat(findMemberB.getMoney()).isEqualTo(10000);
+    }
+
+}
