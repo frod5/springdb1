57073bc5dd4c056a719d2d332179357b98709853
sunwooKim <frod5@kakao.com>
2023-04-12 19:00:29 +0900

    스프링 DB접근기술1 - 자바 예외 처리 - 예외 포함과 스택 트레이스 실습

--- src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -1,11 +1,13 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.net.ConnectException;
 import java.sql.SQLException;
 
+@Slf4j
 public class UnCheckedAppTest {
 
 
@@ -14,6 +16,17 @@ public class UnCheckedAppTest {
         Controller controller = new Controller();
         Assertions.assertThatThrownBy(() -> controller.request())
                 .isInstanceOf(Exception.class); // 이렇게 최상위인 Exception으로 예외를 처리하게되면 상세하게 예외를 처리한 의미가 없어진다.
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            //e.printStackTrace(); 이것은 system.out.print로 나가서 실무에선 사용하지 말자.
+            log.info("ex",e);
+        }
     }
 
     static class Controller{
@@ -48,6 +61,7 @@ public class UnCheckedAppTest {
                 runSql();
             } catch (SQLException e) {
                 throw new RuntimeSQLException(e);  //checked exception을 runtimeException으로 변경하여 처리하여 기존 의존성 문제를 해결.
+//                throw new RuntimeSQLException();  // 파라미터로 원인을 넘겨주지 않으면 예외정보를 알수없음. 실무에서 많이 하는 실수임. 꼭 exception을 넘겨주자
             }
         }
 
